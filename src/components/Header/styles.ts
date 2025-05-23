f073a118c62b5683e4143ba5bdefa0f494de4d52
Lucas Amaral <lucas.trevvos@gmail.com>
2025-05-22 23:04:56 -0300

    feat: header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Image`
+  width: 111px;
+  height: 32px;
+`;
